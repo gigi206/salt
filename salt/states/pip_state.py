8ca9aabd48530351d87eba3d7b81d159be4813a1
Ghislain LE MEUR <gigi206+github@gmail.com>
2020-04-28 19:44:03 +0000

    Fix pip env_vars

--- salt/states/pip_state.py
+++ salt/states/pip_state.py
@@ -313,7 +313,8 @@ def _check_if_installed(prefix,
             available_versions = __salt__['pip.list_all_versions'](
                 prefix, bin_env=bin_env, include_alpha=include_alpha,
                 include_beta=include_beta, include_rc=include_rc, user=user,
-                cwd=cwd, index_url=index_url, extra_index_url=extra_index_url)
+                cwd=cwd, index_url=index_url, extra_index_url=extra_index_url,
+                env_vars=env_vars)
             desired_version = ''
             if any(version_spec):
                 for version in reversed(available_versions):
@@ -716,7 +717,7 @@ def installed(name,
            'comment': '', 'changes': {}}
 
     try:
-        cur_version = __salt__['pip.version'](bin_env)
+        cur_version = __salt__['pip.version'](bin_env, cwd, env_vars)
     except (CommandNotFoundError, CommandExecutionError) as err:
         ret['result'] = None
         ret['comment'] = 'Error installing \'{0}\': {1}'.format(name, err)
@@ -817,7 +818,7 @@ def installed(name,
     else:
         # Attempt to pre-cache a the current pip list
         try:
-            pip_list = __salt__['pip.list'](bin_env=bin_env, user=user, cwd=cwd)
+            pip_list = __salt__['pip.list'](bin_env=bin_env, user=user, cwd=cwd, env_vars=env_vars)
         # If we fail, then just send False, and we'll try again in the next function call
         except Exception as exc:  # pylint: disable=broad-except
             log.exception(exc)
@@ -1050,7 +1051,8 @@ def removed(name,
             timeout=None,
             user=None,
             cwd=None,
-            use_vt=False):
+            use_vt=False,
+            env_vars=None):
     '''
     Make sure that a package is not installed.
 
@@ -1066,7 +1068,7 @@ def removed(name,
     ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
 
     try:
-        pip_list = __salt__['pip.list'](bin_env=bin_env, user=user, cwd=cwd)
+        pip_list = __salt__['pip.list'](bin_env=bin_env, user=user, cwd=cwd, env_vars=env_vars)
     except (CommandExecutionError, CommandNotFoundError) as err:
         ret['result'] = False
         ret['comment'] = 'Error uninstalling \'{0}\': {1}'.format(name, err)
@@ -1090,7 +1092,8 @@ def removed(name,
                                  timeout=timeout,
                                  user=user,
                                  cwd=cwd,
-                                 use_vt=use_vt):
+                                 use_vt=use_vt,
+                                 env_vars=env_vars):
         ret['result'] = True
         ret['changes'][name] = 'Removed'
         ret['comment'] = 'Package was successfully removed.'
@@ -1104,7 +1107,8 @@ def uptodate(name,
              bin_env=None,
              user=None,
              cwd=None,
-             use_vt=False):
+             use_vt=False,
+             env_vars=None):
     '''
     .. versionadded:: 2015.5.0
 
@@ -1126,7 +1130,7 @@ def uptodate(name,
            'comment': 'Failed to update.'}
 
     try:
-        packages = __salt__['pip.list_upgrades'](bin_env=bin_env, user=user, cwd=cwd)
+        packages = __salt__['pip.list_upgrades'](bin_env=bin_env, user=user, cwd=cwd, env_vars=env_vars)
     except Exception as e:  # pylint: disable=broad-except
         ret['comment'] = six.text_type(e)
         return ret
@@ -1140,7 +1144,7 @@ def uptodate(name,
         ret['result'] = None
         return ret
 
-    updated = __salt__['pip.upgrade'](bin_env=bin_env, user=user, cwd=cwd, use_vt=use_vt)
+    updated = __salt__['pip.upgrade'](bin_env=bin_env, user=user, cwd=cwd, use_vt=use_vt, env_vars=env_vars)
 
     if updated.get('result') is False:
         ret.update(updated)
